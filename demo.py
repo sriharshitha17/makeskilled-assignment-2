2bf35ca56b18e93ea7f2414ccf9567ea1317bbe8
sriharshitha17 <sriharshitha.cheruku03@gmail.com>
2023-06-09 12:56:37 +0530

    checked palindrome or not

--- demo.py
+++ demo.py
@@ -2,8 +2,11 @@ a = 'radar'
 b = 'birthday'
 print(type(a),type(b))
 # check - palindrome
-print(a==a[::-1])
-print(b==b[::-1])
-m = 'makeskilled' 
-n = m[1::2] #start index=1,stop index=len(m),step index=2
-print(n)
+if a==a[::-1]:
+    print(a+ ' is a palindrome')
+else:
+    print(a+ ' is not a palindrome')
+if b==b[::-1]:
+    print(b+ ' is a palindrome')
+else:
+    print(b+ ' is not a palindrome')
