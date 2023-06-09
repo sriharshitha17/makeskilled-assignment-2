57d84e3fb3f13dd5a83dfe783c69dec10c210bfc
sriharshitha17 <sriharshitha.cheruku03@gmail.com>
2023-06-09 11:46:38 +0530

    modified hello world to palindrome string

--- demo.py
+++ demo.py
@@ -1,1 +1,9 @@
-print("hello makeskilled")
+a = 'radar'
+b = 'birthday'
+print(type(a),type(b))
+# check - palindrome
+print(a==a[::-1])
+print(b==b[::-1])
+m = 'makeskilled' 
+n = m[1::2] #start index=1,stop index=len(m),step index=2
+print(n)
